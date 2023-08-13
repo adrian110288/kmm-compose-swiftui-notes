92723b77d8ba0fac56f4f56948784116720ef465
Adrian Lesniak <adrian.lesniak@natwest.com>
2023-08-13 13:19:44 +0100

    Upgrade compose and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.2.0-alpha15").apply(false)
     id("com.android.library").version("8.2.0-alpha15").apply(false)
-    kotlin("android").version("1.8.21").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    kotlin("android").version("1.9.0").apply(false)
+    kotlin("multiplatform").version("1.9.0").apply(false)
     id("com.squareup.sqldelight").version("1.5.4").apply(false)
     id("com.google.dagger.hilt.android").version("2.41").apply(false)
 }
